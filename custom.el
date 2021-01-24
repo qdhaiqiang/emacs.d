4db23fde786b28adf0570b63ed5a9b418b1d52b4
mahaiqiang <qdhaiqiang@163.com>
2021-01-24 22:40:32 +0800

    org-mode update

--- custom.el
+++ custom.el
@@ -17,7 +17,6 @@
    (quote
     ("06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "82d2cac368ccdec2fcc7573f24c3f79654b78bf133096f9b40c20d97ec1d8016" "4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4" "4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" "1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" default)))
  '(desktop-save-mode t)
- '(org-agenda-files (quote ("~/git/redcreation/rcpubdoc/早会/record.org")))
  '(package-selected-packages
    (quote
     (uptimes dotenv-mode daemons osx-location dsvn htmlize lua-mode gnuplot flycheck-ledger ledger-mode dash-at-point origami regex-tool info-colors flycheck-clojure cider elein cljsbuild-mode clojure-mode slime-company hippie-expand-slime slime cask-mode cl-libify flycheck-package highlight-quoted macrostep cl-lib-highlight aggressive-indent immortal-scratch auto-compile ipretty elisp-slime-nav paredit-everywhere paredit nginx-mode company-terraform terraform-mode docker-compose-mode dockerfile-mode docker yaml-mode toml-mode flycheck-rust racer rust-mode sqlformat projectile-rails yard-mode bundler goto-gem yari robe ruby-compilation inf-ruby rspec-mode ruby-hash-syntax psci psc-ide reformatter purescript-mode flycheck-elm elm-test-runner elm-mode dhall-mode hindent intero haskell-mode company-anaconda anaconda-mode pip-requirements restclient httprepl haml-mode css-eldoc skewer-less sass-mode rainbow-mode tagedit org-pomodoro writeroom-mode org-cliplink grab-mac-link company-php smarty-mode php-mode add-node-modules-path skewer-mode js-comint xref-js2 prettier-js typescript-mode coffee-mode js2-mode json-mode erlang csv-mode markdown-mode textile-mode cmd-to-echo alert ibuffer-projectile github-review forge github-clone bug-reference-github yagist git-commit magit-todos magit git-timemachine gitconfig-mode gitignore-mode git-blamed vc-darcs darcsum browse-at-remote whitespace-cleanup-mode guide-key highlight-escape-sequences whole-line-or-region move-dup page-break-lines multiple-cursors avy expand-region browse-kill-ring symbol-overlay rainbow-delimiters goto-line-preview beacon mode-line-bell vlf list-unicode-display unfill mmm-mode switch-window company-quickhelp company ivy-xref swiper projectile counsel ivy smex flycheck-color-mode-line flycheck ibuffer-vc wgrep-ag ag wgrep anzu diff-hl diredfl disable-mouse default-text-scale ns-auto-titlebar dimmer color-theme-sanityinc-tomorrow color-theme-sanityinc-solarized command-log-mode scratch diminish exec-path-from-shell fullframe seq pdf-tools try grip-mode flycheck-clj-kondo flycheck-pos-tip ox-reveal org-tree-slide calfw uimage 0blayout org-download yasnippet clojure-snippets markdown-preview-mode clj-refactor neotree))))
@@ -43,6 +42,7 @@
  '((ditaa . t)
    (plantuml . t)
    (dot . t)
+   (python . t)
    (clojure . t)
    ))
 
@@ -115,6 +115,31 @@
 (setq org-todo-keyword-faces '(("TODO" . "red")
                                ("DOING" . "yellow")
                                ("DONE" . "green")))
+
+;;;;插动图片到org 文件时， 自动将文件放到org下的imgs/下，并插入[[file:…imgs/image.jpg]]
+(defun vmacs-org-insert-image (event)
+  (interactive "e")
+  (x-focus-frame nil)
+  (let* ((payload (car (last event)))
+         (type (car payload))
+         (fromname (cadr payload))
+         (img-regexp "\\(gif\\|png\\|jp[e]?g\\)\\>")
+         (destname fromname)
+         img-dir
+         )
+    (when (file-exists-p "../imgs/")
+      (setq img-dir "../imgs/"))
+    (when (file-exists-p "./imgs/")
+      (setq img-dir "./imgs/"))
+    (when (and  (eq 'drag-n-drop (car event))
+                (eq 'file type)
+                (string-match img-regexp fromname)
+                img-dir)
+      (let ((filebasename (file-name-base (buffer-file-name)) ))
+        (setq destname (concat img-dir filebasename "-" (format-time-string "%Y-%m-%d-%H-%M-%S") "." (file-name-extension fromname)))
+        (rename-file fromname destname t))
+      (goto-char (nth 1 (event-start event)))
+      (insert (format "[[file:%s]]" (file-relative-name destname (file-name-directory (buffer-file-name))))))))
 
 ;;;; org-mode setting end
 
